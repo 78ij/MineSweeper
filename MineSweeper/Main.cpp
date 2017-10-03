771f9e9da9f1720214aac746ef13832df8f9261b
Jiamu Sun <x739566858@outlook.com>
2017-10-03 13:55:05 +0800

    Another Explanation.

--- MineSweeper/Main.cpp
+++ MineSweeper/Main.cpp
@@ -1,4 +1,9 @@
 #include"Scene.h"
+//To play:
+//input the density of the mine you want(the value must be between 0 and 1)
+//"#"represents the untouched grid and ghe digits represent the number of mines around this grid
+//input the coordinate of the grid you want to reveal
+//the coordinate starts from 1 and the format is(row,column)
 int main() {
 	double density;
 	std::cout << "please input the density" << std::endl;
@@ -11,7 +16,7 @@ int main() {
 	int row, column;
 	while (true) {
 		std::cin >> row >> column;
-		mine.RevealMine(row, column);
+		mine.RevealMine(row - 1, column - 1);
 		mine.PrintScene();
 		if (mine.gameover) {
 			for (int i = 0; i < 30; i++) {
